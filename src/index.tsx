d1704e33d7d543347c5bee0d49381724de08b1e1
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2024-08-01 20:36:38 +0300

    change baseName

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,12 @@ import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 
+//process.env.PUBLIC_URL
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App />
             </Provider>
